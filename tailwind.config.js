6f23fdc728eee9e7235c169f1c0ee8eaad6dbcda
UmbraPular <DallasKelley1994@protonmail.com>
2022-03-30 12:41:12 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		colors: {
+			PureWhite: '#FFFFFF',
+			Red: '#FC4747',
+			DarkBlue: '#10141E',
+			GreyishBlue: '#FC4747',
+			SemiDarkBlue: '#161D2F',
+		},
+		extend: {
+			fontSize: {
+				L: '2rem',
+				M: '1.5rem',
+				S: '1.5rem',
+				XS: '1.125rem',
+				BodyM: '0.9375rem',
+				BodyS: '0.8125rem',
+			},
+		},
+	},
+	plugins: [],
+};
