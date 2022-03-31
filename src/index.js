342ee852051536e2034d344c8bf3ad4b3fe8a72b
UmbraPular <DallasKelley1994@protonmail.com>
2022-03-31 12:03:01 -0700

    New Initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './containers/Home';
+import Login from './containers/Login';
+import SignUp from './containers/SignUp';
+import Movies from './containers/Movies';
+import TvSeries from './containers/TvSeries';
+import BookMarked from './containers/BookMarked';
 import './styles/index.css';
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='Login' element={<Login />} />
+				<Route path='Sign-Up' element={<SignUp />} />
+				<Route path='Movies' element={<Movies />} />
+				<Route path='Tv-Series' element={<TvSeries />} />
+				<Route path='Bookmarked' element={<BookMarked />} />
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
